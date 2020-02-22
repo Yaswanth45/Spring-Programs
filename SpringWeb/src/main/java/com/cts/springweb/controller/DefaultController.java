630a26fc2d7f01cffb6877637bee016a7c59efd7
T.K. Yaswanth Reddy <52193125+Yaswanth45@users.noreply.github.com>
2020-02-22 02:00:50 -0800

    Spring Web Mvc Program

---
+++ SpringWeb/src/main/java/com/cts/springweb/controller/DefaultController.java
@@ -0,0 +1,22 @@
+package com.cts.springweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController {
+	
+	@RequestMapping({" ","/","/home"})
+	public ModelAndView defaultGetRequestHandler() {
+		ModelAndView mv=new ModelAndView();
+		
+		mv.setViewName("home");
+		
+		mv.addObject("pageTitle","Home Page - Welcome All");
+		mv.addObject("developers",new String[] {"Yaswanth","Dhanu","Siva"});
+		
+		return mv;
+	}
+
+}
